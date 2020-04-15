4812024e13b4d2a89c71768c117eb2e0e01237ac
Vino M SK <vinoth00658@gmail.com>
2020-04-15 15:36:39 +0530

    add quantity ui changes

--- src/components/cards.js
+++ src/components/cards.js
@@ -4,20 +4,30 @@ import { Card, Row, Col, ButtonGroup, Button } from 'react-bootstrap';
 function Cards({itemName, price}) {
 
     const [count, setCount] = useState(0);
+    const [clicked, setClicked] = useState(false);
+    
+    const addButton= () => {
+        setClicked(true);
+    }
 
-        return(
-            <Card body border="light" className="shadow-sm" style={{ height: '9rem', width: '12rem', marginBottom: '10px', margin: '5px' }}>
-                <Row>
-                    <Card.Title><h6>{itemName}</h6></Card.Title>
-                    <Card.Title as={Col}><h6>&#8377; {price}</h6></Card.Title>
-                </Row>
-                <ButtonGroup>
-                    <Button variant="none" onClick={() => count>0? setCount(count - 1): setCount(0)} style={{ borderColor: 'yellow', boxShadow: 'none', marginTop: "30px" }}>-</Button>
-                    <Button variant="none" style={{ borderColor: 'yellow', boxShadow: 'none', marginTop: "30px" }}>{count}</Button>
-                    <Button variant="none" onClick={() => count>=0? setCount(count + 1): setCount(0)} style={{ borderColor: 'yellow', boxShadow: 'none', marginTop: "30px" }}>+</Button>
-                </ButtonGroup>                                
-            </Card>
-        );
+    return(
+        <Card body border="light" className="shadow-sm" style={{ height: '9rem', width: '12rem', marginBottom: '10px', margin: '5px' }}>
+            <Row>
+                <Card.Title><h6>{itemName}</h6></Card.Title>
+                <Card.Title as={Col}><h6>&#8377; {price}</h6></Card.Title>
+            </Row>
+            {   clicked === false ? 
+                <Button variant="none" onClick={addButton} style={{ borderColor: 'yellow', boxShadow: 'none', marginTop: "30px" }}>Add</Button>:
+                <>
+                    <ButtonGroup>
+                        <Button variant="none" onClick={() => count>0? setCount(count - 1): setCount(0)} style={{ borderColor: 'yellow', boxShadow: 'none', marginTop: "30px" }}>-</Button>
+                        <Button variant="none" style={{ borderColor: 'yellow', boxShadow: 'none', marginTop: "30px" }}>{count}</Button>
+                        <Button variant="none" onClick={() => count>=0? setCount(count + 1): setCount(0)} style={{ borderColor: 'yellow', boxShadow: 'none', marginTop: "30px" }}>+</Button>
+                    </ButtonGroup>                 
+                </>
+            }
+        </Card>
+    );
 }
 
 export default Cards;
