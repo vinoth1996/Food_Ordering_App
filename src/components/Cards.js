a6adf56e776728da28c9968a5b8509024e209923
Vino M SK <vinoth00658@gmail.com>
2020-04-18 12:19:06 +0530

    included the decrease and increase quantity function

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -6,8 +6,16 @@ function Cards({itemName, price}) {
     const [count, setCount] = useState(0);
     const [clicked, setClicked] = useState(false);
     
-    const addButton= () => {
+    const addButton = () => {
         setClicked(true);
+    }
+
+    const decreaseQuantity = () => {
+        let quantity = count > 0? setCount(count - 1): setCount(0)
+    }
+
+    const increaseQuantity = () => {
+        let quantity = count>=0? setCount(count + 1): setCount(0)
     }
 
     return(
@@ -20,9 +28,9 @@ function Cards({itemName, price}) {
                 <Button variant="none" onClick={addButton} style={{ borderColor: 'yellow', boxShadow: 'none', marginTop: "30px", borderRadius: "25px", padding: "7px 30px" }}>Add</Button>:
                 <>
                     <ButtonGroup>
-                        <Button variant="none" onClick={() => count>0? setCount(count - 1): setCount(0)} style={{ borderColor: 'yellow', boxShadow: 'none', marginTop: "30px" }}>-</Button>
+                        <Button variant="none" onClick={decreaseQuantity} style={{ borderColor: 'yellow', boxShadow: 'none', marginTop: "30px" }}>-</Button>
                         <Button variant="none" style={{ borderColor: 'yellow', boxShadow: 'none', marginTop: "30px" }}>{count}</Button>
-                        <Button variant="none" onClick={() => count>=0? setCount(count + 1): setCount(0)} style={{ borderColor: 'yellow', boxShadow: 'none', marginTop: "30px" }}>+</Button>
+                        <Button variant="none" onClick={increaseQuantity} style={{ borderColor: 'yellow', boxShadow: 'none', marginTop: "30px" }}>+</Button>
                     </ButtonGroup>                 
                 </>
             }
